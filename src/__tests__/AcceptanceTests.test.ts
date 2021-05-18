c124d8a11bae5739705b7be0a5b84e7a4790e16f
George Harris <citizenharris@gmail.com>
2021-05-18 11:00:45 +0100

    Refactor personalised greeting acceptance test into a parameterised test for new or existing users

--- src/__tests__/AcceptanceTests.test.ts
+++ src/__tests__/AcceptanceTests.test.ts
@@ -13,66 +13,44 @@ import PoolHandler from '../PoolHandler'
 import EventListenerController from '../EventListenerController'
 
 describe("Slack Service should", () => {
-    const mockedSlackApiClient: SlackApiClient = mock(SlackApiClient);
 
-    const identityResponse = {
-        "ok": true,
-        "user": {
-            "name": "Joe Bloggs",
-            "id": "U0G9QF9C6",
-            "email": "joe.bloggs@codurance.com"
-        },
-        "team": {
-            "id": "T0G9PQBBK"
-        }
-    };
-
-    const newUserPayload = {
-        "type": "member_joined_channel",
-        "user": "U0G9QF9C6",
-        "channel": "C0698JE0H",
-        "channel_type": "C",
-        "team": "T024BE7LD",
-        "inviter": "U123456789"
-    };
-
-    test("send a welcome message to new users", () => {
+    it.each`
+        isNewUser | expectedMessage
+        ${true}   | ${'Hi Joe Bloggs, welcome to Sessionize!'} 
+        ${false}  | ${'Hi Joe Bloggs, welcome back to Sessionize!'}
+    `("send a personalised message when a user joins the channel", ({ isNewUser, expectedMessage }) => {
 
         // GIVEN Sessionize is installed
         // WHEN a user joins the Sessionize slack channel
         // THEN they receive a personalized welcome message
 
         // Arrange
-        const expectedMessage = "Hi Joe Bloggs, welcome to Sessionize!";
+        const identityResponse = {
+            "ok": true,
+            "user": {
+                "name": "Joe Bloggs",
+                "id": "U0G9QF9C6",
+                "email": "joe.bloggs@codurance.com"
+            },
+            "team": {
+                "id": "T0G9PQBBK"
+            }
+        };
+
+        const newUserPayload = {
+            "type": "member_joined_channel",
+            "user": "U0G9QF9C6",
+            "channel": "C0698JE0H",
+            "channel_type": "C",
+            "team": "T024BE7LD",
+            "inviter": "U123456789"
+        };
 
         const mockedCoreApiClient: CoreApiClient = mock(CoreApiClient);
         const coreApiClient: CoreApiClient = instance(mockedCoreApiClient);
-        when(mockedCoreApiClient.isNewUser(identityResponse)).thenReturn(true);
+        when(mockedCoreApiClient.isNewUser(identityResponse)).thenReturn(isNewUser);
 
-        const slackApiClient: SlackApiClient = instance(mockedSlackApiClient);
-        when(mockedSlackApiClient.getIdentity(newUserPayload["user"])).thenReturn(identityResponse);
-        when(mockedSlackApiClient.sendDm(newUserPayload["user"], anyString())).thenReturn(identityResponse);
-
-        const messageBuilder = new MessageBuilder();
-        const poolHandler = new PoolHandler(coreApiClient, slackApiClient, messageBuilder);
-        const eventListenerController = new EventListenerController(poolHandler);
-
-        // Act
-        eventListenerController.joinPool(newUserPayload);
-
-        // Assert
-        verify(mockedCoreApiClient.isNewUser(identityResponse)).once();
-        verify(mockedSlackApiClient.sendDm(identityResponse["user"]["id"], expectedMessage)).once();
-    });
-
-    test("send a welcome back message to returning users", () =>  {
-
-        const expectedMessage = "Hi Joe Bloggs, welcome back to Sessionize!";
-
-        const mockedCoreApiClient: CoreApiClient = mock(CoreApiClient);
-        const coreApiClient: CoreApiClient = instance(mockedCoreApiClient);
-        when(mockedCoreApiClient.isNewUser(identityResponse)).thenReturn(false);
-
+        const mockedSlackApiClient: SlackApiClient = mock(SlackApiClient);
         const slackApiClient: SlackApiClient = instance(mockedSlackApiClient);
         when(mockedSlackApiClient.getIdentity(newUserPayload["user"])).thenReturn(identityResponse);
         when(mockedSlackApiClient.sendDm(newUserPayload["user"], anyString())).thenReturn(identityResponse);
