3e3cc0baa3df387d197d5cea3cc2cb9b9feb9bfa
George Harris <citizenharris@gmail.com>
2021-05-19 13:53:56 +0100

    Rewrite acceptance tests for receiving a message after joining a channel

--- src/JoinChannel/ChannelEventHandler.ts
+++ src/JoinChannel/ChannelEventHandler.ts
@@ -1,9 +1,8 @@
-import { MemberJoinedChannelEvent } from "@slack/bolt";
+import {MemberJoinedChannelEvent} from "@slack/bolt";
 import CoreApiClient from "../CoreApiClient"
 import MessageBuilder from "../MessageBuilder"
 import SlackApiClient from "../SlackApiClient"
 import SlackUserIdentity from "../SlackUserIdentity";
-import JoinChannelEvent from "./JoinChannelEvent";
 
 export default class ChannelEventHandler {
 
@@ -20,10 +19,8 @@ export default class ChannelEventHandler {
     async onChannelJoin(newUserPayload: MemberJoinedChannelEvent){
         const slackIdentity: SlackUserIdentity = await this.slackApiClient.getIdentity(newUserPayload.user);
 
-        const message = this.coreApiClient.isNewUser(slackIdentity)
+        return this.coreApiClient.isNewUser(slackIdentity)
             ? this.messageBuilder.buildGreeting(slackIdentity.name)
             : this.messageBuilder.buildWelcomeBack(slackIdentity.name);
-
-        return message;
     }
 }
