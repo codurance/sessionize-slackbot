39677c77e0dcbb3d2d8c95fa1a6744f73e315c3d
George Harris <citizenharris@gmail.com>
2021-05-21 09:35:14 +0100

    Spike a core api client

--- src/CoreApiClient.ts
+++ src/CoreApiClient.ts
@@ -4,8 +4,26 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export default class CoreApiClient {
-    isNewUser(slackUserIdentity: SlackUserIdentity): boolean {
-        if(process.env.MOCK_CORE) return true;
-        throw new Error("Method not implemented.");
+    async isNewUser(slackUserIdentity: SlackUserIdentity): Promise<boolean> {
+        const response = await this.post("https://sessionizertest.azurewebsites.net/isNewUser", {
+            body: slackUserIdentity
+        })
+
+        return response.json();
+        // throw new Error("Method not implemented.");
+    }
+
+    private post = async (url: string, options : any) => {
+        return await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(options.body)
+        })
     }
 }
