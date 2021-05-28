69ef7e7ab17691739d9f0d211f087278c1caebb1
craw-codurance <cameron.raw@codurance.com>
2021-05-28 09:26:08 +0200

    moved bootstrap code into function closure and tidied route handler invocations

--- src/main.ts
+++ src/main.ts
@@ -7,62 +7,50 @@ import SlackApiClient from './SlackApiClient';
 import ApiEventHandler from './EventHandlers/ApiEventHandler';
 import express from 'express';
 
-dotenv.config()
-
-const coreApiClient = new CoreApiClient();
-const slackApiClient = new SlackApiClient();
-const messageBuilder = new MessageBuilder();
-
-const channelEventHandler = new ChannelEventHandler(coreApiClient, slackApiClient, messageBuilder);
-const apiEventHandler = new ApiEventHandler(coreApiClient, slackApiClient, messageBuilder);
-
-
-const receiver = new ExpressReceiver({
-    signingSecret: process.env.SLACK_SIGNING_SECRET!
-});
-
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    receiver
-});
-
-app.event('member_joined_channel', async ({ event }) => {
-    console.log(event);
-    await channelEventHandler.onChannelJoin(event);
-});
-
-app.event('member_left_channel', async ({ event }) => {
-    console.log(event);
-    channelEventHandler.onChannelLeave(event);
-});
-
-app.action('approve_session', async ({ ack, say }) => {
-    console.log("A session was approved!");
-    await ack();
-    await say("Thanks! You have successfully accepted this match!");
-});
-
-receiver.router.use(express.json());
-receiver.router.use(express.urlencoded({ extended: true }));
-
-receiver.router.post('/direct-message', (req, res) => {
-  apiEventHandler.onDirectMessage(req, res);
-});
-
-receiver.router.post('/match-notification', (req, res) => {
-    apiEventHandler.onMatchNotification(req, res);
-});
-
-receiver.router.post('/language-preferences', (req, res) => {
-    apiEventHandler.onLanguagePreferences(req, res);
-});
-
-receiver.router.post('/slack/interactive-endpoint', (req, res) => {
-    channelEventHandler.interactiveMessageResponse(req, res);
-});
-
-
 (async () => {
+    dotenv.config()
+
+    const coreApiClient = new CoreApiClient();
+    const slackApiClient = new SlackApiClient();
+    const messageBuilder = new MessageBuilder();
+    
+    const channelEventHandler = new ChannelEventHandler(coreApiClient, slackApiClient, messageBuilder);
+    const apiEventHandler = new ApiEventHandler(coreApiClient, slackApiClient, messageBuilder);
+    
+    
+    const receiver = new ExpressReceiver({
+        signingSecret: process.env.SLACK_SIGNING_SECRET!
+    });
+    
+    const app = new App({
+        token: process.env.SLACK_BOT_TOKEN,
+        receiver
+    });
+    
+    app.event('member_joined_channel', async ({ event }) => {
+        console.log(event);
+        await channelEventHandler.onChannelJoin(event);
+    });
+    
+    app.event('member_left_channel', async ({ event }) => {
+        console.log(event);
+        channelEventHandler.onChannelLeave(event);
+    });
+    
+    app.action('approve_session', async ({ ack, say }) => {
+        console.log("A session was approved!");
+        await ack();
+        await say("Thanks! You have successfully accepted this match!");
+    });
+    
+    receiver.router.use(express.json());
+    receiver.router.use(express.urlencoded({ extended: true }));
+    
+    receiver.router.post('/direct-message', apiEventHandler.onDirectMessage);
+    receiver.router.post('/match-notification', apiEventHandler.onMatchNotification);
+    receiver.router.post('/language-preferences', apiEventHandler.onLanguagePreferences);
+    receiver.router.post('/slack/interactive-endpoint', channelEventHandler.interactiveMessageResponse);
+        
     await app.start(80);
     console.log("Sessionize SlackBot running");
 
